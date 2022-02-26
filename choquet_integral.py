1e438b242b31a785990b97e6be9145b9eda628e7
ViniRezende <100439234+ViniRezende@users.noreply.github.com>
2022-02-26 11:26:16 -0300

    Update choquet_integral.py

--- choquet_integral.py
+++ choquet_integral.py
@@ -18,20 +18,20 @@ def choquetintegral(P, u):
   n = len(P[0]) #Número de critérios
   p = np.arange(1,n+1) #Vetor 1,2...n para ser utilizado como suporte para obter todas as combinações em ordem lexicográfica
   S = np.concatenate([np.pad(np.array(list(itertools.combinations(p, r))), ((0,0),(0,n-r)), 'constant', constant_values=0) for r in range(0,n+1)], axis=0)
-    #^ Matriz que contem todas as combinações em ordem lexicográfica   
+    #S: Matriz que contem todas as combinações em ordem lexicográfica   
 
   #Etapa 2) Processo de ordenamento das alternativas em ordem crescente e coletando e ordenando seus índices 
-  P_aumentado = np.pad(P, ((0,0),(0,1)), 'constant', constant_values=0) #Aumenta a matriz de decisão e acrescenta uma coluna de zeros
-  P_new = np.sort(P_aumentado) #Ordenamento da matriz aumentada
-  P_indice = np.argsort(P) + 1  #Mantendo os índices da ordenação acrescidos de 1 para dar match com a matris S
+  P_aumentado = np.pad(P, ((0,0),(0,1)), 'constant', constant_values=0)
+  P_new = np.sort(P_aumentado) 
+  P_indice = np.argsort(P) + 1  
 
-  M = np.zeros((len(P), 2**n)) #Matriz de zeros para ser substituida pelos valores de  P_new
+  M = np.zeros((len(P), 2**n))
 
   #Etapa 3) Cálculo da Integral de Choquet
-  for i in range(0,len(P)): #Roda para todas as linhas (alternativas)
-    for j in range(1, n+1): #Interaçoes que acontecem nas colunas
-      P_indice_aux = np.pad(np.sort(P_indice[i][j-1:n]), ((0,j-1)), 'constant', constant_values=0)  #Obtendo p/ cada interação os índices ordenados acrescidos de zeros
-      M[i][np.flatnonzero(np.all(S== P_indice_aux, 1))] = P_new[i][j] - P_new[i][j-1] #Substituindo onde da Match entre S e P_indice_aux na matrix M os valores de P_new
+  for i in range(0,len(P)): 
+    for j in range(1, n+1):
+      P_indice_aux = np.pad(np.sort(P_indice[i][j-1:n]), ((0,j-1)), 'constant', constant_values=0)  
+      M[i][np.flatnonzero(np.all(S== P_indice_aux, 1))] = P_new[i][j] - P_new[i][j-1]
 
   M  #Matriz de interesse
   CI = np.dot(M, u)
